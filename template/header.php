2895dabf747d26bf49385f64bd07619141a40459
iamrjb11 <iamrjb11@gmail.com>
2018-10-09 15:15:01 +0600

    First upload

---
+++ template/header.php
@@ -0,0 +1,35 @@
+<?php
+include_once "../template/resourcesFile.php";
+
+?>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="http://localhost/framework/ui/home.php">Mr. Chase</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li class="active"><a href="http://localhost/framework/ui/home.php">Home</a></li>
+	      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="http://localhost/framework/ui/diseaseInfo.php">Disease Info</span></a>
+
+	      </li>
+	      <li><a href="http://localhost/framework/ui/doctor_list.php">Doctor List</a></li>
+	      <li><a href="http://localhost/framework/ui/donor_list.php">Blood Donor List</a></li>
+	    </ul>
+	    <ul class="nav navbar-nav navbar-right">
+	      <li><a href="http://localhost/framework/ui/singup.php">Sign Up</a></li>
+	      <li><a href="http://localhost/framework/ui/login.php">
+	      <?php
+	      ?>
+	      Login</a></li>
+	    </ul>
+	  </div>
+	</nav>
+
+
+
+</body>
